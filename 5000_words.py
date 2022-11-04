9752ed10be606fddfc565d7d7a8f33e2d1cdeccb
Stasensus <stanislav@kireev.ru>
2022-11-04 17:24:20 +0300

    Test commit

--- 5000_words.py
+++ 5000_words.py
@@ -1,6 +1,6 @@
 FILE = r'C:\Users\USER\PycharmProjects\Bot\5000_words.txt'
 FILE2 = r'C:\Users\USER\PycharmProjects\Bot\file.txt'
-list = []
+
 with open (FILE, encoding="utf8") as f:
     line = f.read()
     while line:
@@ -9,14 +9,21 @@ with open (FILE, encoding="utf8") as f:
                      'u', 'v', 'x', 'y', 'z']:
                 with open (FILE2, 'a') as file:
                     file.write(i)
-            elif i in ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
-                       'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']:
+            elif i == ';':
                 with open(FILE2, 'a') as file:
                     file.write("\n")
+        break
 
+with open (FILE2) as f:
+    list = f.readlines()
+    list = [line.rstrip('\n') for line in list]
+
+print(list)
 print('Я закончил')
 
+['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+#            'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']:
 
-        # list.append(line)
-        # print(list)
-        # line = f.read()
+
+
+
